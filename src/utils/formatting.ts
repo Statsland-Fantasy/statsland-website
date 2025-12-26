3b519b567d219372006f610ed341c8fe860153cb
wesleychipeco <wescollege36@gmail.com>
2025-12-25 22:30:07 -0800

    Fix bug where timezone was not being respected so the dates were mismatching

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -20,11 +20,20 @@ export const formatDate = (dateString: string | undefined): string => {
   if (!dateString) {
     return "";
   }
+
+  // Check if date is already in YYYY-MM-DD format as should be from DB
+  const yyyymmddPattern = /^(\d{4})-(\d{2})-(\d{2})$/;
+  const match = dateString.match(yyyymmddPattern);
+  if (match) {
+    const [, year, month, day] = match;
+    return `${month}-${day}-${year.slice(-2)}`;
+  }
+
   try {
     const date = new Date(dateString);
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const year = String(date.getFullYear()).slice(-2);
+    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+    const day = String(date.getUTCDate()).padStart(2, "0");
+    const year = String(date.getUTCFullYear()).slice(-2);
     return `${month}-${day}-${year}`;
   } catch {
     return dateString;
